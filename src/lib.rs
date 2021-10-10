4064e59f6a0830a5c20f141cd0515ea4b6eaa02c
Simone Giacomelli <simone.giacomelli@gmail.com>
2021-10-10 09:56:44 +0200

    dom manipulation

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,20 @@ pub fn greet() {
 fn main() {
     println!("ciao")
 }
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello from lib.rs!"));
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
